d611c424eff0dea9162e9bb05a9eaea6223b5ff9
Loviit <alyssa.binard@viacesi.fr>
2018-06-04 09:15:53 +0200

    Commentaire sur Sprites

--- model/src/main/java/model/mobile/Sprite.java
+++ model/src/main/java/model/mobile/Sprite.java
@@ -3,6 +3,13 @@ package model.mobile;
 import java.awt.Image;
 import javax.swing.ImageIcon;
 
+/**
+ * <h1>The Class Sprite give different Sprite.</h1>
+ *
+ * @author Alyssa BINARD
+ * @version 1.0
+ */
+
 public class Sprite {
 
     protected int x;
@@ -23,35 +30,63 @@ public class Sprite {
         this.y = y;
         visible = true;
     }
-
+    
+    
+    /**
+     * Get Image with loadImage.
+     * @param imageName
+     */
     protected void loadImage(String imageName) {
 
         ImageIcon ii = new ImageIcon(imageName);
         image = ii.getImage();
     } 
     
+    /**
+     * Get Image Dimensions
+     */
     protected void getImageDimensions() {
 
         width = image.getWidth(null);
         height = image.getHeight(null);
     }    
 
+    /**
+     * Get Image.
+     * @return image
+     */
     public Image getImage() {
         return image;
     }
 
+    /**
+     * Get X
+     * @return X
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Get Y.
+     * @return Y
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Booelan isVisible
+     * @return visible
+     */
     public boolean isVisible() {
         return visible;
     }
 
+    /**
+     * Set visible
+     * @param visible
+     */
     public void setVisible(Boolean visible) {
         this.visible = visible;
     }
