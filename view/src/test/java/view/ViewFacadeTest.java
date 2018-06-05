1f807f2294ebed2474dcbec3ab46e73409182b7b
Loviit <alyssa.binard@viacesi.fr>
2018-06-05 14:24:56 +0200

    ne fonctionne pas car Anthony n'a pas séparé le frame et panel

---
+++ view/src/test/java/view/ViewFacadeTest.java
@@ -0,0 +1,30 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ViewFacadeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+}
