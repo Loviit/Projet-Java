baeb57f50a9349df95ebdeefe04d61d790882ec4
Loviit <alyssa.binard@viacesi.fr>
2018-06-05 14:26:39 +0200

    javadoc

--- view/src/test/java/view/ViewFacadeTest.java
+++ view/src/test/java/view/ViewFacadeTest.java
@@ -4,16 +4,44 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
+/**
+ * <h1>The test for View.</h1>
+ * 
+ * @author Alyssa BINARD
+ * @version 1.0
+ * @see assertEquals
+ * @see After
+ * @see AfterClass
+ * @see Before
+ * @see BeforeClass
+ * @see Test
+ */
 public class ViewFacadeTest {
-
+	/**
+	 * (non-javadoc) setUpBeforeClass
+	 * 
+	 * @throws Exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
 
+	/**
+	 * (non-javadoc) tearDownAfterClass
+	 * 
+	 * @throws Exception
+	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
+
+	/**
+	 * setUp
+	 * 
+	 * @throws Exception
+	 */
 
 	@Before
 	public void setUp() throws Exception {
